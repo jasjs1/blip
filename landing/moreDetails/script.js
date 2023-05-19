6836adc828bf5acb7ab09cdae6efff7e728402b9
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-05-18 20:12:57 -0500

    added code

---
+++ landing/moreDetails/script.js
@@ -0,0 +1,32 @@
+function saveSignupInfo() {
+    var form = document.getElementById('form');
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+  
+      var name = document.getElementById('name').value;
+      var username = document.getElementById('username').value;
+      var mainInterests = document.getElementById('main-interests').value;
+      var profileImageInput = document.getElementById('profile-image');
+      var profileImage = '';
+  
+      if (profileImageInput.files.length > 0) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+          profileImage = event.target.result;
+          saveDataToLocalStorage(name, username, mainInterests, profileImage);
+        };
+        reader.readAsDataURL(profileImageInput.files[0]);
+      } else {
+        saveDataToLocalStorage(name, username, mainInterests, profileImage);
+      }
+    });
+  
+    function saveDataToLocalStorage(name, username, mainInterests, profileImage) {
+      localStorage.setItem('name', name);
+      localStorage.setItem('username', username);
+      localStorage.setItem('mainInterests', mainInterests);
+      localStorage.setItem('profileImage', profileImage);
+      window.location.href = 'https://www.example.com';
+    }
+  }
+  
