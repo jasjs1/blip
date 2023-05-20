25d12fc133a1d31dfdf920acfbb7bec5938c5dfd
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-05-20 13:15:05 -0500

    add redirect to settings code + first cm

---
+++ profile/script.js
@@ -0,0 +1,5 @@
+function showSettings() {
+    console.log('Redirecting to settings...')
+    console.log('Redreict pending: to settings...')
+    window.location.href = 'http://127.0.0.1:5500/profile/dashboard/profile-dashboard.html';
+}
