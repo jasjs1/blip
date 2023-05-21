1870ba9a2fea78004c3e6253fb3807936f43bbdc
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-05-21 15:24:24 -0500

    added font weight and updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
         'c1': '#1c1f23',
         'c2': '#4BA3E3',
       },
+      fontWeight: {
+        thin: '100',
+        hairline: '100',
+        extralight: '200',
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+        'extra-bold': '800',
+        black: '900',
+      }
+    },
+    colors: {
+      'secondary': '#5D97CF',
     },
   },
   variants: {},
